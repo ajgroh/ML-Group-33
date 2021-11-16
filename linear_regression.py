3aa0e474fadfbb1df8348e81aaca5192a1f6d0db
rorbrow <roryalanbrownjr@icloud.com>
2021-11-16 09:06:11 -0500

    Add files via upload

---
+++ linear_regression.py
@@ -0,0 +1,44 @@
+#ML Project
+import pandas as pd
+import numpy as np
+import csv
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression, Ridge
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+file_name = "NFLData.csv"
+
+df = pd.read_csv(file_name)
+df = df.sample(frac=1) #random ordering of the data points
+x = df.to_numpy()[:, 0:12]
+# x = preprocessing.normalize(x)
+y = df.to_numpy()[:, 12]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
+# print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
+
+
+
+
+############# Linear Regression ################
+max_iter = 300
+model = Ridge(alpha=.1, max_iter= max_iter)
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+print(model.score(x_train, y_train))
+
+print(y_test)
+print(y_pred)
+
+# cm = confusion_matrix(y_test, y_pred, labels=[0., 1.])
+# disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0., 1.])
+# disp.plot()
+# plt.show()
+
+
+# print("Accuracy = ", metrics.accuracy_score(y_test, y_pred))
+# y_pred_prob = reg.predict_proba(x_test)[:,1]
+# auc = metrics.roc_auc_score(y_test, y_pred_prob)
+# print("AUC:", round(auc, 2))
