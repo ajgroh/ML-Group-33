5e833fd1adf57487550e011fe6f816744a9fa932
Rory Brown <rorybrownjr@Rorys-MBP.localdomain>
2021-12-06 23:32:06 -0500

    class creation

--- linear_regression.py
+++ linear_regression.py
@@ -1,42 +1,45 @@
 # ML Project
-import pandas as pd
-import numpy as np
-import csv
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression, Ridge
-from sklearn import metrics
-from sklearn import preprocessing
+from sklearn.linear_model import Ridge
+
 from sklearn.metrics import r2_score, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
-file_name = "./Data/MoreNFLData.csv"
-df = pd.read_csv(file_name)
-df = df.sample(frac=1)  # random ordering of the data points
-x = df.to_numpy()[:, 0:13]
-# x = preprocessing.normalize(x)
-y = df.to_numpy()[:, 13]
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.25, random_state=42)
-# print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
+# file_name = "./Data/MoreNFLData.csv"
+# df = pd.read_csv(file_name)
+# df = df.sample(frac=1)  # random ordering of the data points
+# x = df.to_numpy()[:, 0:13]
+# # x = preprocessing.normalize(x)
+# y = df.to_numpy()[:, 13]
 
 
-############# Linear Regression ################
-max_iter = 30000
-model = Ridge(alpha=.01, max_iter=max_iter)
-model.fit(x_train, y_train)
-y_pred = model.predict(x_test)
-print(r2_score(y_test, y_pred))
+class LinearRegression:
+    def __init__(self, x, y, max_iter=30000):
+        self.x = x
+        self.y = y
+        self.max_iter = max_iter
 
-# print(y_test)
-# print(y_pred)
+    def main(self):
+        x_train, x_test, y_train, y_test = train_test_split(
+            self.x, self.y, test_size=0.25, random_state=42)
+        # print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
 
-# cm = confusion_matrix(y_test, y_pred, labels=[0., 1.])
-# disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0., 1.])
-# disp.plot()
-# plt.show()
+        ############# Linear Regression ################
 
+        model = Ridge(alpha=.01, max_iter=self.max_iter)
+        model.fit(x_train, y_train)
+        y_pred = model.predict(x_test)
+        print(r2_score(y_test, y_pred))
 
-# print("Accuracy = ", metrics.accuracy_score(y_test, y_pred))
-# y_pred_prob = reg.predict_proba(x_test)[:, 1]
-# auc = metrics.roc_auc_score(y_test, y_pred_prob)
-# print("AUC:", round(auc, 2))
+        # print(y_test)
+        # print(y_pred)
+
+        # cm = confusion_matrix(y_test, y_pred, labels=[0., 1.])
+        # disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0., 1.])
+        # disp.plot()
+        # plt.show()
+
+        # print("Accuracy = ", metrics.accuracy_score(y_test, y_pred))
+        # y_pred_prob = reg.predict_proba(x_test)[:, 1]
+        # auc = metrics.roc_auc_score(y_test, y_pred_prob)
+        # print("AUC:", round(auc, 2))
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,7 +1,5 @@
 # ML Project
 import pandas as pd
-import numpy as np
-import csv
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
@@ -9,37 +7,51 @@ from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
-file_name = "./Data/MoreNFLData.csv"
-df = pd.read_csv(file_name)
-df = df.sample(frac=1)  # random ordering of the data points
-x = df.to_numpy()[:, 0:13]
+# file_name = "./Data/MoreNFLData.csv"
+# df = pd.read_csv(file_name)
+# df = df.sample(frac=1)  # random ordering of the data points
+# x = df.to_numpy()[:, 0:13]
 
-# Scale data before applying PCA
-scaling = preprocessing.StandardScaler()
+# # Scale data before applying PCA
+# scaling = preprocessing.StandardScaler()
 
-# Use fit and transform method
-scaling.fit(x)
-Scaled_data = scaling.transform(x)
+# # Use fit and transform method
+# scaling.fit(x)
+# Scaled_data = scaling.transform(x)
 
-y = df.to_numpy()[:, 13]
-x_train, x_test, y_train, y_test = train_test_split(
-    Scaled_data, y, test_size=0.25, random_state=42)
-print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
+# y = df.to_numpy()[:, 13]
 
 
-############# Logistic Regression ################
-num_iters = 10000
-reg = LogisticRegression(max_iter=num_iters)
-reg.fit(x_train, y_train)
-y_pred = reg.predict(x_test)
-# print(type(reg.classes_))
-cm = confusion_matrix(y_test, y_pred, labels=reg.classes_)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=reg.classes_)
-disp.plot()
-plt.show()
+class LogisticRegression():
+    def __init__(self, x, y, max_iter=10000):
+        self.x = x
+        self.y = y
+        self.max_iter = max_iter
 
+    def main(self):
+        x_train, x_test, y_train, y_test = train_test_split(
+            self.x, self.y, test_size=0.25, random_state=42)
+        # print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
 
-print("Accuracy = ", metrics.accuracy_score(y_test, y_pred))
-y_pred_prob = reg.predict_proba(x_test)[:, 1]
-auc = metrics.roc_auc_score(y_test, y_pred_prob)
-print("AUC:", round(auc, 2))
+        ############# Logistic Regression ################
+        num_iters = 10000
+        reg = LogisticRegression(max_iter=num_iters)
+        reg.fit(x_train, y_train)
+
+        # print(type(reg.classes_))
+
+        return reg, x_test, y_test
+
+    def plot(self, model, x_test, y_test):
+
+        y_pred = model.predict(x_test)
+        cm = confusion_matrix(y_test, y_pred, labels=model.classes_)
+        disp = ConfusionMatrixDisplay(
+            confusion_matrix=cm, display_labels=model.classes_)
+        disp.plot()
+        plt.show()
+
+        print("Accuracy = ", metrics.accuracy_score(y_test, y_pred))
+        y_pred_prob = model.predict_proba(x_test)[:, 1]
+        auc = metrics.roc_auc_score(y_test, y_pred_prob)
+        print("AUC:", round(auc, 2))
